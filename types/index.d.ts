dd33b835d2611214910790785161ced47b20cbb0
Голуб Дмитрий <dmytrygolub@gmail.com>
2024-01-05 13:44:33 +0200

    Add pipeline

---
+++ types/index.d.ts
@@ -0,0 +1,7 @@
+import {Application} from "../src/core/Application";
+import APIError from "../src/types/APIError";
+import {Client} from "../src/types/Client";
+import {Endpoint} from "../src/types/Endpoint";
+import {Message} from "../src/types/Message";
+
+declare module "@dmytryG/websocket-rush-api";
