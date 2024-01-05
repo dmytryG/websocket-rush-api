593ca7e052a2a431173c380103106905b58e2fc8
Голуб Дмитрий <dmytrygolub@gmail.com>
2024-01-05 15:49:11 +0200

    Trying to fix error with main

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import {Application} from "../src/core/Application";
+import APIError from "../src/types/APIError";
+import {Client} from "../src/types/Client";
+import {Endpoint} from "../src/types/Endpoint";
+import {Message} from "../src/types/Message";
+
+export { Application, APIError, Client, Endpoint, Message }
