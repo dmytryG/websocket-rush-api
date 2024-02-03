c752155dd42be8275609e64c58f0d9bd0a5c4828
Голуб Дмитрий <dmytrygolub@gmail.com>
2024-02-04 00:04:34 +0200

    Update version, minor fixes

--- src/types/Preprocessor.ts
+++ src/types/Preprocessor.ts
@@ -1,4 +1,4 @@
-import {Message} from "~/types/Message";
-import {Application} from "~/core/Application";
+import {Message} from "../types/Message";
+import {Application} from "../core/Application";
 
 export type Preprocessor = (req: Message, application: Application) => Promise<any>;
