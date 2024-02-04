cf217a087dd3ed7c3ff690b2cee5fd2160d2d1fc
Голуб Дмитрий <dmytrygolub@gmail.com>
2024-02-04 12:45:22 +0200

    Add logger disabling

--- src/core/Logger.ts
+++ src/core/Logger.ts
@@ -1,10 +1,22 @@
 import * as console from "console";
 
 export default class Logger {
+    private static _isEnabled = true;
+
+    static get isEnabled(): boolean {
+        return this._isEnabled;
+    }
+
+    static set isEnabled(value: boolean) {
+        this._isEnabled = value;
+    }
+
     static async log(...args: any[]): Promise<void> {
+        if (!Logger.isEnabled) return
         console.log((new Date()).toISOString(), ...args)
     }
     static async error(...args: any[]): Promise<void> {
+        if (!Logger.isEnabled) return
         console.error((new Date()).toISOString(), ...args)
     }
 }
